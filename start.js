078f35450574a04dd34a006f4690dff69d55c5a7
Aakash Kumar <137307053+HackerAku@users.noreply.github.com>
2023-12-03 10:53:45 +0530

    Add files via upload

---
+++ start.js
@@ -0,0 +1,18 @@
+// console.log("Hello World!")
+// //This is a comment
+// let num=2;
+// if (num%2===0){
+//     console.log("Even");
+// }
+// else{
+//     console.log("Odd");
+// // }
+// let ak=prompt("eneter you name");
+// console.log(ak);
+// let num=prompt("enter a number");
+// if (num%5===0){
+//     console.log("Multiple of 5")
+// }
+// else{
+//     console.log("Not a multiple")
+// }
